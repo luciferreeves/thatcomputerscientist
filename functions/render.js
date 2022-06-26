82e67e49f4e15c3e543c49450ab12111723f09c9
Bobby <bobbyskhs@gmail.com>
2022-06-26 22:02:17 +0530

    fix domain based URLs

--- functions/render.js
+++ functions/render.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const validationString = process.env.AUTHORIZATION_STRING;
 function renderRoute(req, res, page, title, protected = false, data = {}) {
   res.locals.messages = req.flash();
+  let currentDomain = req.get("host").split(".");
+  currentDomain = req.protocol + "://" + currentDomain.at(-2) + "." + currentDomain.at(-1);
   jwt.verify(req.cookies.token, validationString, (err, decoded) => {
     if (err) {
       res.clearCookie("token");
@@ -12,6 +14,7 @@ function renderRoute(req, res, page, title, protected = false, data = {}) {
         res.render(page, {
           title: title,
           ...data,
+          domain: currentDomain
         });
       }
     } else {
@@ -19,6 +22,7 @@ function renderRoute(req, res, page, title, protected = false, data = {}) {
         title: title,
         username: decoded.username,
         ...data,
+        domain: currentDomain
       });
     }
   });
--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,8 @@ router.use('/api', api);
 router.use('/account', account);
 router.use('/_profile', profile);
 router.get('*', (req, res) => {
-    renderRoute(req, res, "404", "Page Not Found");
+    // renderRoute(req, res, "404", "Page Not Found");
+    res.status(404).send('404');
 });
 
 module.exports = router;

7c8887bcf7da4a1e33f46fd09676454c47018561
Bobby <bobbyskhs@gmail.com>
2022-06-28 02:38:06 +0530

    Revert: Render Route for Profile Page

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const {renderRoute} = require("../functions/render");
 
 router.get('/:username', (req, res) => {
-    // res.send(`Hello ${req.params.username}`);
-    renderRoute(req, res, "index", "Profile");
+    res.send(`Hello ${req.params.username}`);
+    // renderRoute(req, res, "index", "Profile");
 });
 
 module.exports = router;

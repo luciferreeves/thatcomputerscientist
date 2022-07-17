b31bd3dbabb57eda3fddfaa7a59e01e73442834f
Bobby <bobbyskhs@gmail.com>
2022-07-18 00:57:36 +0530

    Change Init to Ghost

--- routes/profile.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const {renderRoute} = require("../functions/render");
-
-router.get('/:username', (req, res) => {
-    res.send(`Hello ${req.params.username}`);
-    // renderRoute(req, res, "index", "Profile");
-});
-
-module.exports = router;

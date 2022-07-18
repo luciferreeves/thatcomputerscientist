450c9a65cd3490eabf6a4b2a9efaa92fb1284080
Bobby <bobbyskhs@gmail.com>
2022-07-18 10:49:37 +0530

    Allowing all hosts temporarily

--- thatcomputerscientist/settings.py
+++ thatcomputerscientist/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure--8^hnd!ymuui4c2hu0$x9_j@eylb2tpr1xiq@xr2a(2vfwc=2_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

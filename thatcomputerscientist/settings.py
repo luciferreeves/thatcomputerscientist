82cc3c55fc5dd6329f934b70934a68756e9f6e24
Bobby <bobbyskhs@gmail.com>
2022-07-18 12:22:31 +0530

    Test: holding sessions through subdomains

--- thatcomputerscientist/settings.py
+++ thatcomputerscientist/settings.py
@@ -31,6 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://*.thatcomputerscientist.com', 'https://*.thatcomputerscientist.fly.dev/']
+SESSION_COOKIE_DOMAIN = ".thatcomputerscientist.com"
+DOMAIN_NAME = "thatcomputerscientist.com"
 
 # Application definition
 

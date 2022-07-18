57a57fce04eaa54933ba5f195e3c130f9d7b9aa5
Bobby <bobbyskhs@gmail.com>
2022-07-18 12:05:06 +0530

    Removed SSL Option from Database

--- thatcomputerscientist/settings.py
+++ thatcomputerscientist/settings.py
@@ -85,12 +85,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {
-            'ssl': {
-                'ca': os.getenv('MYSQL_ATTR_SSL_CA'),
-            }
-        }
+        'PORT': os.getenv('DB_PORT')
     }
 }
 

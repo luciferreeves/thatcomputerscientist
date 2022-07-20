88057646dbd4876e0658e2e1ee35756c73db607a
Bobby <bobbyskhs@gmail.com>
2022-07-20 20:20:30 +0530

    Change Admin area settings

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,15 @@
 from multiprocessing.dummy import Namespace
 from django.urls import path
 from . import views
+from django.contrib import admin
 
 app_name = 'users'
 urlpatterns = [
     path('login', views.login_user, name='login'),
     path('logout', views.logout_user, name='logout'),
 ]
+
+# Configure Admin Site
+admin.site.site_header = 'That Computer Scientist Administation'
+admin.site.site_title = 'That Computer Scientist'
+admin.site.index_title = 'Administration Area'

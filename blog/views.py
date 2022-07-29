b14dc2abc25e59e66237fef8d9c2b55ad9592dcf
Bobby <bobbyskhs@gmail.com>
2022-07-29 23:01:55 +0530

    Account Page with Fetched Information from the database

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from users.models import UserProfile
+import hashlib
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html', {'title': 'Home'})
+
+def account(request):
+    user = request.user
+    try:
+        user_profile = UserProfile.objects.get(user=user)
+        avatar = hashlib.md5(str(user_profile.gravatar_email).lower().encode('utf-8')).hexdigest() if user_profile.gravatar_email else hashlib.md5(str(user.email).lower().encode()).hexdigest()
+    except UserProfile.DoesNotExist:
+        user_profile = None
+        avatar = hashlib.md5(str(user.email).lower().encode()).hexdigest()
+    return render(request, 'account.html', {'title': 'Account', 'user_profile': user_profile, 'avatar': avatar})
--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+# User Profile Model
+class UserProfile(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    location = models.CharField(max_length=50, blank=True)
+    bio = models.TextField(blank=True)
+    gravatar_email = models.EmailField(blank=True)
+    is_public = models.BooleanField(default=False)
+    email_public = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+        

2081b4a4ab1521022b1fa702e5647204379992d6
Bobby <bobbyskhs@gmail.com>
2022-08-02 01:03:16 +0530

    Added userpages app

--- thatcomputerscientist/settings.py
+++ thatcomputerscientist/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'users',
+    'userpages',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +61,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'thatcomputerscientist.urls'
+
 
 TEMPLATES = [
     {

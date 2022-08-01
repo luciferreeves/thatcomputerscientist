6e4a5984db8311a15dc31a22b21ded01eec278de
Bobby <bobbyskhs@gmail.com>
2022-08-02 01:08:04 +0530

    Moved urlparsing to request.scheme while sending user url

--- blog/views.py
+++ blog/views.py
@@ -16,7 +16,7 @@ def account(request):
             avatar = hashlib.md5(str(user_profile.gravatar_email).lower().encode('utf-8')).hexdigest() if user_profile.gravatar_email else hashlib.md5(str(user.email).lower().encode()).hexdigest()
             user_subdomain_url = None
             if user_profile.is_public:
-                scheme = urlparse(request.build_absolute_uri()).scheme
+                scheme = request.scheme if request.scheme else 'http'
                 domain = urlparse(request.build_absolute_uri()).netloc
                 user_subdomain_url = '{}://{}.{}'.format(scheme, user.username, domain)
         except UserProfile.DoesNotExist:

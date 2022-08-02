09fc14c16b6b9ba4435b8b329a5b9a25c38b8836
Bobby <bobbyskhs@gmail.com>
2022-08-02 19:45:12 +0530

    Basic Hello World Message on subdomain

--- userpages/views.py
+++ userpages/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, world. You're at the home page.")
+    return HttpResponse('Hello, world. You are at the home page.')

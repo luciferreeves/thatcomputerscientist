4b2cb153d4a43d9cd52bb4975af757d6ce193f3d
Bobby <bobbyskhs@gmail.com>
2022-08-03 00:01:41 +0530

    Define rules for static

--- thatcomputerscientist/urls.py
+++ thatcomputerscientist/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,5 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
     path('', include(('userpages.urls', 'userpages'), namespace='userpages')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

5ab9a8355296b28565e8a39d6b8fff0cbb504932
Bobby <bobbyskhs@gmail.com>
2022-08-03 00:02:04 +0530

    Added views for user home page

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ import hashlib
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'title': 'Home'})
+    return render(request, 'blog/home.html', {'title': 'Home'})
 
 def account(request):
     user = request.user
@@ -23,10 +23,10 @@ def account(request):
         except UserProfile.DoesNotExist:
             user_profile = None
             avatar = hashlib.md5(str(user.email).lower().encode()).hexdigest()
-        return render(request, 'account.html', {'title': 'Account', 'user_profile': user_profile, 'avatar': avatar, 'user_subdomain_url': user_subdomain_url})
+        return render(request, 'blog/account.html', {'title': 'Account', 'user_profile': user_profile, 'avatar': avatar, 'user_subdomain_url': user_subdomain_url})
     else:
         # Redirect to login page
         return redirect('/')
 
 def homepage(request):
-    return render(request, 'homepage.html', {'title': 'Homepage'})
+    return render(request, 'blog/homepage.html', {'title': 'Homepage'})

1a7bc7ab002b75dc3a8d8f780fb71d1691eb2816
Bobby <bobbyskhs@gmail.com>
2022-08-29 13:11:08 -0400

    Fix Account Redirection

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('my/', RedirectView.as_view(pattern_name='account', permanent=False)),
+    path('account/', RedirectView.as_view(pattern_name='account', permanent=False)),
     path('my/account', views.account, name='account'),
     path('my/homepage', views.homepage, name='homepage'),
 ]

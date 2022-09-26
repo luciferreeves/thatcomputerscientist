7e11c94866ff0da2a4eb30734bd0e24a18010951
Bobby <bobbyskhs@gmail.com>
2022-09-25 20:16:51 -0400

    Fixed some positionings and display first paragraph of top 5 articles on home

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,7 +1,9 @@
 from .models import Post, Category
 
 def recent_posts():
-    recent_posts = Post.objects.filter(is_public=True).order_by('-date')[1:6]
+    recent_posts = Post.objects.filter(is_public=True).order_by('-date')[:5]
+    for post in recent_posts:
+        post.excerpt = post.body.split('>')[1].split('<')[0]
     return recent_posts
 
 def categories():

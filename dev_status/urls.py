fb1225d85d9291a394e9b371231b02a85272d121
Bobby <bobbyskhs@gmail.com>
2022-10-02 11:18:46 -0400

    source code rendered through a single function

--- dev_status/urls.py
+++ dev_status/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 app_name = 'dev_status'
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('tree/', views.tree, name='roottree'),
-    path('tree/<path:path>', views.tree, name='tree'),
-    path('raw/<path:path>', views.raw, name='raw'),
+    path('<str:r>', views.home, name='repo'),
+    path('<str:r>/<path:p>', views.home, name='repo-path'),
+    path('', RedirectView.as_view(url='thatcomputerscientist'))
 ]
--- dev_status/views.py
+++ dev_status/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render
 from github import Github
 from dotenv import load_dotenv
@@ -7,46 +8,27 @@ load_dotenv()
 
 # Create your views here.
 
-def home(request):
+def home(request, r='thatcomputerscientist', p=None):
     g = Github(os.getenv('GH_TOKEN'))
-    repo = g.get_repo('luciferreeves/thatcomputerscientist')
-    contents = repo.get_contents('')
-    files = []
-    while contents:
-        file_content = contents.pop(0)
-        files.append(file_content)
-    context = {
-        'title': 'Source Code',
-        'files': files,
-    }
-    return render(request, 'dev_status/home.html', context)
-
-def tree(request, path=None):
-    g = Github(os.getenv('GH_TOKEN'))
-    repo = g.get_repo('luciferreeves/thatcomputerscientist')
-    path = '' if not path else path
-    parent = '' if len(path.split('/')) == 1 else '/'.join(path.split('/')[:-1])
-    contents = repo.get_contents(path)
-    files = []
-    while contents:
-        file_content = contents.pop(0)
-        files.append(file_content)
-    context = {
-        'title': 'Tree - {}'.format(path),
-        'files': files,
-        'parent': parent,
-    }
-    return render(request, 'dev_status/home.html', context)
-
-def raw(request, path):
-    g = Github(os.getenv('GH_TOKEN'))
-    repo = g.get_repo('luciferreeves/thatcomputerscientist')
-    path = '' if not path else path
-    parent = '' if len(path.split('/')) == 1 else '/'.join(path.split('/')[:-1])
-    contents = repo.get_contents(path)
-    context = {
-        'title': 'File - {}'.format(path),
-        'file': contents.html_url,
-        'parent': parent,
-    }
-    return render(request, 'dev_status/home.html', context)
+    repository = 'luciferreeves/{}'.format(r)
+    parent = None
+    if p and not len(p.split('/')) == 0:
+        parent = '/'.join(p.split('/')[:-1])
+    contents = g.get_repo(repository).get_contents(p or '')
+    context = {}
+    try:
+        files = []
+        while contents:
+            file_content = contents.pop(0)
+            files.append(file_content)
+        context['title'] = 'Tree - {}'.format(p)
+        context['files'] = files
+        context['parent'] = parent
+        context['repo'] = r
+    except:
+        context['title'] = 'File - {}'.format(p)
+        context['file'] = contents.html_url
+        context['parent'] = parent
+        context['repo'] = r
+    finally:
+        return render(request, 'dev_status/home.html', context)

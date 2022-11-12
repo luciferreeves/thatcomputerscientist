b7aa1d8c57b70acccff704e4763eadd8a4a84900
Bobby <bobbyskhs@gmail.com>
2022-11-12 13:41:17 -0500

    fixed few IE5 render issues, announcements auto add now false

--- announcements/models.py
+++ announcements/models.py
@@ -1,16 +1,22 @@
+from django.utils import timezone
 from django.db import models
 from django.conf import settings
 # Create your models here.
 
 class Announcement(models.Model):
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField()
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
     is_public = models.BooleanField(default=False)
     is_new = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.created_at = timezone.now()
+        return super(Announcement, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.content

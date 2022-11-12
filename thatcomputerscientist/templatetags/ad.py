68ab1b4dc4ce0c3a5467fd76cb96d76d5f970904
Bobby <bobbyskhs@gmail.com>
2022-11-12 04:33:29 -0500

    fixing fake ads on browsers

--- thatcomputerscientist/templatetags/ad.py
+++ thatcomputerscientist/templatetags/ad.py
@@ -8,10 +8,8 @@ register = template.Library()
 
 @register.filter(name='ad')
 def ad(type):
-    path = 'static/images/ads/{}'.format(type)
+    path = 'static/images/fakeads/{}'.format(type)
     files = os.listdir(path)
     file = random.choice(files)
-    with open('{}/{}'.format(path, file), 'rb') as f:
-        data = base64.b64encode(f.read()).decode('utf-8')
-        ext = file.split('.')[-1]
-        return 'data:image/{};base64,{}'.format(ext, data)
+    # return path
+    return "/static/images/fakeads/{}/{}".format(type, file)
--- thatcomputerscientist/urls.py
+++ thatcomputerscientist/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),

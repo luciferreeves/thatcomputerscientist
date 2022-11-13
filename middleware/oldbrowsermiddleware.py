85531d778d82da8dc9992e499bee7111958a554b
Bobby <bobbyskhs@gmail.com>
2022-11-13 10:28:08 -0500

    middleware and css fixes for older browsers

---
+++ middleware/oldbrowsermiddleware.py
@@ -0,0 +1,37 @@
+import re
+class OldBrowserMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user_agent = request.META.get('HTTP_USER_AGENT', '').lower()
+        old_browser = False
+        onclick = True
+        browser_patterns = [
+            'msie [1-8]',
+            'firefox/[1-3]\.',
+            'chrome/[1-9]\.',
+            'safari/[1-5]\.',
+            'opera/[1-9]\.',
+            'classilla'
+        ]
+        onclick_patterns = [
+            'classilla'
+        ]
+        for pattern in browser_patterns:
+            if re.search(pattern, user_agent):
+                old_browser = True
+                break
+        
+        for pattern in onclick_patterns:
+            if re.search(pattern, user_agent):
+                onclick = False
+                break
+        
+        request.old_browser = old_browser
+        request.onclick = onclick
+        response = self.get_response(request)
+        print("old_browser: ", request.old_browser)
+        print("onclick: ", request.onclick)
+        return response
+        
--- thatcomputerscientist/settings.py
+++ thatcomputerscientist/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'middleware.oldbrowsermiddleware.OldBrowserMiddleware',
 ]
 
 CONFIGURED_SUBDOMAINS = {

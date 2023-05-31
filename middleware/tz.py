20a385c69f91414701f42b4a514e77c2f6b6b270
Bobby <bobbyskhs@gmail.com>
2023-05-31 19:02:14 -0400

    TZ Middleware __call__ return a definite output

--- middleware/tz.py
+++ middleware/tz.py
@@ -13,20 +13,20 @@ class TimezoneMiddleware(object):
     def __call__(self, request):
         # get the user's timezone from the cookie
         user_timezone = request.COOKIES.get('user_timezone')
-
+        remote_ip = request.META.get('REMOTE_ADDR')
         if not user_timezone:
             x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-            
             if x_forwarded_for:
                 remote_ip = x_forwarded_for.split(',')[0]
             else:
                 remote_ip = request.META.get('REMOTE_ADDR')
         
-            geo_data = requests.get(f'http://ip-api.com/json/{remote_ip}').json()
-            user_timezone = geo_data['timezone'] if 'timezone' in geo_data else 'UTC'
+        geo_data = requests.get(f'http://ip-api.com/json/{remote_ip}').json()
+        user_timezone = geo_data['timezone'] if 'timezone' in geo_data else 'UTC'
 
-            if user_timezone:
-                response = self.get_response(request)
-                response.set_cookie('user_timezone', user_timezone, max_age=31536000)
-                return response
-            return self.get_response(request)
+        if user_timezone:
+            response = self.get_response(request)
+            response.set_cookie('user_timezone', user_timezone)
+            return response
+        
+        return self.get_response(request)

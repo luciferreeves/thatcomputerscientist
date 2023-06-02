f3c455d198ef5aaed09efd7b197bc815f183f7b9
Bobby <bobbyskhs@gmail.com>
2023-06-02 15:03:08 -0400

    Users Broken

--- middleware/uuidmiddleware.py
+++ middleware/uuidmiddleware.py
@@ -26,36 +26,44 @@ def userTrackingContextProcessor(request):
     # get the user's uuid from the cookie
     user_uuid = request.COOKIES.get('user_uuid')
 
-    if user_uuid:
-        # get the user's permissions
-        is_authenticated = request.user.is_authenticated
-        is_staff = request.user.is_staff
+    print(user_uuid)
 
-        # refresh online users every 300 seconds, with auto deleting expired keys
-        cache.set(f"presence_{user_uuid}", {
-            'is_authenticated': is_authenticated,
-            'is_staff': is_staff,
-        }, 300)
+    return {
+        'anonymous_users': 0,
+        'logged_in_users':0,
+        'admin_users': 0,
+    }
 
-        # get all online users
-        online_now = cache.keys('presence_*')
+    # if user_uuid:
+    #     # get the user's permissions
+    #     is_authenticated = request.user.is_authenticated
+    #     is_staff = request.user.is_staff
 
-        # separate online users into anonymous, logged in, and admin users
-        anonymous_users = []
-        logged_in_users = []
-        admin_users = []
+    #     # refresh online users every 300 seconds, with auto deleting expired keys
+    #     cache.set(f"presence_{user_uuid}", {
+    #         'is_authenticated': is_authenticated,
+    #         'is_staff': is_staff,
+    #     }, 300)
 
-        for user in online_now:
-            user_data = cache.get(user)
-            if user_data['is_authenticated'] == False and user_data['is_staff'] == False:
-                anonymous_users.append(user_data)
-            elif user_data['is_authenticated'] == True and user_data['is_staff'] == False:
-                logged_in_users.append(user_data)
-            if user_data['is_staff'] == True:
-                admin_users.append(user_data)
+    #     # get all online users
+    #     online_now = cache.keys('presence_*')
 
-        return {
-            'anonymous_users': len(anonymous_users),
-            'logged_in_users': len(logged_in_users),
-            'admin_users': len(admin_users),
-        }
+    #     # separate online users into anonymous, logged in, and admin users
+    #     anonymous_users = []
+    #     logged_in_users = []
+    #     admin_users = []
+
+    #     for user in online_now:
+    #         user_data = cache.get(user)
+    #         if user_data['is_authenticated'] == False and user_data['is_staff'] == False:
+    #             anonymous_users.append(user_data)
+    #         elif user_data['is_authenticated'] == True and user_data['is_staff'] == False:
+    #             logged_in_users.append(user_data)
+    #         if user_data['is_staff'] == True:
+    #             admin_users.append(user_data)
+
+    #     return {
+    #         'anonymous_users': len(anonymous_users),
+    #         'logged_in_users': len(logged_in_users),
+    #         'admin_users': len(admin_users),
+    #     }

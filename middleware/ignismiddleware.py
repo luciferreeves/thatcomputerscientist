164950f013807651da5dca224a70fadef6986953
Bobby <30593201+luciferreeves@users.noreply.github.com>
2024-07-24 15:31:21 +0000

    127.0.0.1 excluded from local ignis; custom scrollbar in code blocks

--- middleware/ignismiddleware.py
+++ middleware/ignismiddleware.py
@@ -25,8 +25,8 @@ class IgnisMiddleware(MiddlewareMixin):
     def __call__(self, request):
         response = self.get_response(request)
 
-        # id request is from localhost, do not process
-        if re.match(r"^localhost", request.get_host()):
+        # id request is from localhost or 127.0.0.1, do not process
+        if re.match(r"^localhost", request.get_host()) or re.match(r"^127.0.0.1", request.get_host()):
             return response
 
         # Do not process non-HTML responses

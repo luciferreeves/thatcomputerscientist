5b7fd58969c5c91c552e744f2bb8349e89608511
Bobby <bobbyskhs@gmail.com>
2025-05-20 09:24:16 +0530

    complete left sidebar

--- middleware/i18nmiddleware.py
+++ middleware/i18nmiddleware.py
@@ -10,6 +10,8 @@ class I18NMiddleware(MiddlewareMixin):
         else:
             activate("en")
         request.LANGUAGE_CODE = language
+        request.ALT_LANGUAGE = "ja" if language == "en" else "en"    
+    
 
     def process_response(self, request, response):
         if not request.COOKIES.get("site_language"):

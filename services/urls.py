68a0b4bad10d4aeedbb4b5629b69619ee807b869
Bobby <bobbyskhs@gmail.com>
2025-09-19 05:02:24 +0530

    fixed urls in global service urls

--- services/urls.py
+++ services/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path, include
 
-from services.pamphlet import views as pamphlet_views
-from services.kawaiibeats import views as kawaiibeats_views
-
 app_name = "services"
 urlpatterns = [
     path("/journals", include("services.journals.urls")),
-    path("/kawaiibeats", kawaiibeats_views.random_song, name="kawaiibeats_random_song"),
-    path("/pamphlet", pamphlet_views.pamphlet, name="pamphlet"),
+    path("/kawaiibeats", include("services.kawaiibeats.urls")),
+    path("/pamphlet", include("services.pamphlet.urls")),
 ]
